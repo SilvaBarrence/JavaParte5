33bf4d575d6507024603e2ca4a21796f2e09b0cb
Gabriel <g.dessilva95@gmail.com>
2020-09-04 22:08:40 -0300

    Pacotes e java.lang - Criando uma biblioteca com JAR

---
+++ bytebank-biblioteca/src/br/com/alura/bytebank/TesteBiblioteca.java
@@ -0,0 +1,14 @@
+package br.com.alura.bytebank;
+
+import br.com.bytebank.banco.modelo.Conta;
+import br.com.bytebank.banco.modelo.ContaCorrente;
+
+public class TesteBiblioteca {
+
+    public static void main(String[] args) {
+         Conta c = new ContaCorrente(123, 234);
+         c.deposita(300.0);
+
+        System.out.println(c.getSaldo());
+    }
+}
